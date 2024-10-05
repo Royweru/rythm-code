8d81048b11b61ac45852fe6b28a9fed939a78829
Royweru <royezi17@gmail.com>
2024-10-05 09:39:41 +0300

    modified:   components/hero/index.tsx

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -20,7 +20,7 @@ export const Hero = ({
           className=" w-full relative lg:p-20 md:p-16 p-6  md:grid lg:grid-cols-2
          md:grid-cols-2 grid-cols-1 gap-5 hidden "
         >
-          <div className="  h-full flex-col justify-center items-center hidden md:flex col-span-1">
+          <div className="  h-full flex-col justify-center items-center hidden lg:flex col-span-1">
             <h2 className="max-w-lg font-bold font-gothic text-shade-yellow lg:text-[2.75rem] text-4xl relative leading-tight">
               Welcome to Rythm code where we solve your coding problems
             </h2>
@@ -30,7 +30,7 @@ export const Hero = ({
           </div>
           <InputBox courses={courses} />
         </div>
-        <div className=" w-full relative flex flex-col gap-y-4 md:hidden">
+        <div className=" w-full relative flex flex-col gap-y-4 lg:hidden">
           <div className=" w-full flex items-center justify-center">
             <button className="px-8 py-0.5  border-2 border-black dark:border-white uppercase bg-white text-black transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] w-[300px]"
             onClick={()=>onOpen(courses)}
